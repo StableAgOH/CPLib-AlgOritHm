cd840e227252f2a6c50524c1a3b56f53af715fc6
StableAgOH <stagoh17@gmail.com>
2022-06-04 16:48:10 +0800

    feat(algo): add phi & add exeuler

--- lib/algo/algo.hpp
+++ lib/algo/algo.hpp
@@ -2,6 +2,7 @@
 #define CPLIB_ALGORITHM_LIB_ALGO_ALGO_HPP
 
 #include <iterator>
+#include <string>
 #include <vector>
 
 namespace agoh
@@ -19,7 +20,7 @@ int64_t exgcd(int64_t a, int64_t b, int64_t& x, int64_t& y)
     return d;
 }
 
-int64_t qpow(int64_t a, int64_t k, int64_t p)
+int64_t qpow(int64_t a, int64_t k, int32_t p)
 {
     int64_t res = 1;
     for(; k; k >>= 1, a = a * a % p)
@@ -27,20 +28,54 @@ int64_t qpow(int64_t a, int64_t k, int64_t p)
     return res;
 }
 
-int64_t inv(int64_t a, int64_t p)
+int64_t inv(int64_t a, int32_t p)
 {
     int64_t x, y;
     exgcd(a, p, x, y);
     return (x % p + p) % p;
 }
 
-std::vector<int64_t> invn(size_t n, int64_t p)
+std::vector<int64_t> invn(size_t n, int32_t p)
 {
     std::vector<int64_t> inv(n + 1);
     inv[1] = 1;
     for(size_t i = 2; i <= n; i++)
         inv[i] = (p - p / static_cast<int64_t>(i)) * inv[p % i] % p;
     return inv;
+}
+
+int64_t phi(int64_t x)
+{
+    auto res = x;
+    for(int64_t i = 2; i * i <= x; i++)
+    {
+        if(x % i == 0)
+        {
+            res = res / i * (i - 1);
+            while(x % i == 0)
+                x /= i;
+        }
+    }
+    if(x > 1) res = res / x * (x - 1);
+    return res;
+}
+
+int32_t exeuler(int64_t a, std::string k, int32_t p)
+{
+    auto phip = phi(p);
+    int64_t t = 0;
+    bool flag = false;
+    for(auto c : k)
+    {
+        t = t * 10 + c - '0';
+        if(t > phip)
+        {
+            t %= phip;
+            flag = true;
+        }
+    }
+    if(flag) t += phip;
+    return qpow(a, t, p);
 }
 
 std::vector<int32_t> euler_seive_prime(size_t n)
--- test/algo/algoTest.cpp
+++ test/algo/algoTest.cpp
@@ -30,6 +30,24 @@ TEST(algoTest, algoTest_invn)
     ASSERT_EQ(agoh::invn(10, 13), vans);
 }
 
+TEST(algoTest, algoTest_phi)
+{
+    ASSERT_EQ(agoh::phi(1), 1);
+    ASSERT_EQ(agoh::phi(5), 4);
+    ASSERT_EQ(agoh::phi(100), 40);
+    ASSERT_EQ(agoh::phi(347), 346);
+    ASSERT_EQ(agoh::phi(500), 200);
+}
+
+TEST(algoTest, algoTest_exeuler)
+{
+    ASSERT_EQ(agoh::exeuler(9, "8", 7), 4);
+    ASSERT_EQ(agoh::exeuler(998244353, "98765472103312450233333333333", 12345), 5333);
+    ASSERT_EQ(agoh::exeuler(12345, "12345678987654321", 12345678), 2699055);
+    ASSERT_EQ(agoh::exeuler(2, "1", 4), 2);
+    ASSERT_EQ(agoh::exeuler(2, "2", 4), 0);
+}
+
 TEST(algoTest, algoTest_euler_seive_prime)
 {
     std::vector<int32_t> vans{2,  3,  5,  7,  11, 13, 17, 19, 23, 29, 31, 37, 41,
